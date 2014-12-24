f7cc09413cc5c2ba90848ddfe380fa3a7ff8c884
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-25 03:32:24 +0530

    refactored server code

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ Entries = new Meteor.Collection('entries');
 Meteor.methods({
   addEntry: function(name, phoneNo) {
     Entries.insert({name: name, phoneNo: phoneNo});
-  },
+  }
+});
 
+Meteor.methods({
   getEntries: function(searchText) {
     var regExp = new RegExp(searchText, 'i');
     return Entries.find({
