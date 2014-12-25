e1ebff81e35963c909252430a1f35a382f56a6cd
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2014-12-25 05:44:54 +0530

    added final solution

--- server/app.js
+++ server/app.js
@@ -1,16 +1,64 @@
+// loading the npm module
+ElasticSearch = Meteor.npmRequire('elasticsearch');
+
+// create the connection
+EsClientSource = new ElasticSearch.Client({
+  host: 'localhost:9200'
+});
+
+// make it fiber aware 
+EsClient = Async.wrap(EsClientSource, ['search', 'create']);
+
 Entries = new Meteor.Collection('entries');
 
 Meteor.methods({
   addEntry: function(name, phoneNo) {
-    Entries.insert({name: name, phoneNo: phoneNo});
+    var id = Entries.insert({name: name, phoneNo: phoneNo});
+
+    // create a document index Elastic Search
+    EsClient.create({
+      index: "myindex",
+      type: "phonebook",
+      id: id,
+      body: {
+        name: name,
+        phoneNo: phoneNo
+      }
+    });
   }
 });
 
 Meteor.methods({
   getEntries: function(searchText) {
-    var regExp = new RegExp(searchText, 'i');
-    return Entries.find({
-      name: regExp
-    }, {sort: {name: 1}, limit:20}).fetch();
+    var lastWord = searchText.trim().split(" ").splice(-1)[0];
+    var query = {
+      "bool": {
+        "must": [
+          {
+            "bool": {
+              "should": [
+                {"match": {"name": {"query": searchText}}},
+                {"prefix": {"name": lastWord}}
+              ]
+            }
+          }
+        ],
+        "should": [
+          {"match_phrase_prefix": {"name": {"query": searchText, slop: 5}}}
+        ]
+      }
+    };
+
+    var result =  EsClient.search({
+      index: "myindex",
+      type: "phonebook",
+      body: {
+        query: query
+      }
+    });
+
+    return result.hits.hits.map(function(doc) {
+      return doc._source;
+    });
   }
 });
